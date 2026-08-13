a3f06aebf2e54fcac60b96e37fb1c92bd9feeee5
SigmaKriper007 <orcyl753@gmail.com>
2026-08-13 18:59:45 +0300

    first commit

---
+++ src/client/java/com/opus/client/OpusVsExeClient.java
@@ -0,0 +1,10 @@
+package com.opus.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OpusVsExeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
